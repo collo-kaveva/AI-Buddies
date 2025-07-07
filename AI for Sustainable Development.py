bbd48647bc165d06175306bf9ce9e6c6614dc455
Collins Kaveva <145143450+collo-kaveva@users.noreply.github.com>
2025-07-07 03:35:24 +0300

    Add files via upload

---
+++ AI for Sustainable Development.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+plt.scatter(y_test, linear_predictions, color='blue', label='Linear Regression')
+plt.scatter(y_test, rf_predictions, color='green', label='Random Forest')
+plt.xlabel('Actual Emissions')
+plt.ylabel('Predicted Emissions')
+plt.legend()
+plt.show()
+   
